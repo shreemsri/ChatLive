24182aa4bf76eabb27cec29b717408527fefcad4
shreemsri <shreemsrivastava2005@gmail.com>
2025-11-24 19:35:49 +0530

    Connect client to deployed backend

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://chatlive-op54.onrender.com", {
+const socket = io("https://chatlive-server.onrender.com", {
   transports: ["websocket"],
 });
 
